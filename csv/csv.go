5cecbf144305e4c658f26a873152ece2a05e0c77
stepupdream <stepupdream1002@gmail.com>
2022-10-17 23:46:13 +0900

    [update] Add isArrayUnique.

--- csv/csv.go
+++ csv/csv.go
@@ -132,3 +132,17 @@ func GetCSVFilePaths(path string) ([]string, error) {
 
 	return paths, err
 }
+
+func isArrayUnique(args []int) bool {
+	encountered := map[int]bool{}
+	count := len(args)
+	for i := 0; i < count; i++ {
+		if !encountered[args[i]] {
+			encountered[args[i]] = true
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
