1b925639ee03b95f7d9aae1b803b11bed4e0b8b9
stepupdream <stepupdream1002@gmail.com>
2022-10-22 17:27:40 +0900

    [fix] Change of affiliation package.

--- array/array.go
+++ array/array.go
@@ -2,7 +2,6 @@ package array
 
 import (
 	"log"
-	"os"
 )
 
 func StrContains(slice []string, target string) bool {
@@ -69,26 +68,6 @@ func NextArrayValue(allValues []string, nowValue string) string {
 	}
 
 	return allValues[nowKey+1]
-}
-
-func GetDirectoryNames(path string) []string {
-	dir, err := os.Open(path)
-	if err != nil {
-		log.Fatal("Not found : ", err)
-	}
-	defer func(dir *os.File) {
-		err := dir.Close()
-		if err != nil {
-
-		}
-	}(dir)
-
-	names, err := dir.Readdirnames(-1)
-	if err != nil {
-		log.Fatal("ReadDirError: ", err)
-	}
-
-	return names
 }
 
 func SliceString(all []string, start string, end string) []string {
--- directory/directory.go
+++ directory/directory.go
@@ -1,6 +1,9 @@
 package directory
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func ExistDirectory(path string) bool {
 	_, err := os.Stat(path)
@@ -8,4 +11,24 @@ func ExistDirectory(path string) bool {
 		return false
 	}
 	return true
+}
+
+func GetDirectoryNames(path string) []string {
+	dir, err := os.Open(path)
+	if err != nil {
+		log.Fatal("Not found : ", err)
+	}
+	defer func(dir *os.File) {
+		err := dir.Close()
+		if err != nil {
+
+		}
+	}(dir)
+
+	names, err := dir.Readdirnames(-1)
+	if err != nil {
+		log.Fatal("ReadDirError: ", err)
+	}
+
+	return names
 }
