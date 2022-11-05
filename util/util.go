1b9a6e651594eb6fc4294a7ab0ed3fda2419e9cd
stepupdream <stepupdream1002@gmail.com>
2022-11-06 03:53:29 +0900

    [add] Add ProgressBar.

--- util/util.go
+++ util/util.go
@@ -4,9 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 func KeyWait(message string) {
 	fmt.Println(message)
 	bufio.NewScanner(os.Stdin).Scan()
+}
+
+func StartProgressBar(totalCount int) *pb.ProgressBar {
+	bar := pb.Simple.Start(totalCount)
+	bar.SetMaxWidth(80)
+
+	return bar
 }
