0bcd46f393773334e9f94f39c3660b293ace4f58
stepupdream <stepupdream1002@gmail.com>
2022-11-06 01:41:56 +0900

    [update] Remove methods that are not appropriate for commonality.

--- excel/excel.go
+++ excel/excel.go
@@ -1,19 +1,14 @@
 package excel
 
 import (
-	"fmt"
 	"io/fs"
 	"log"
 	"path/filepath"
-	"reflect"
 
 	"github.com/pkg/errors"
-	"github.com/stepupdream/golang-support-tool/csv"
-	"github.com/stepupdream/golang-support-tool/file"
-	"github.com/xuri/excelize/v2"
 )
 
-func GetFilePathRecursive(path string) ([]string, error) {
+func GetFilePathRecursive(path string) []string {
 	var paths []string
 
 	// Recursively retrieve directories and files. (use WalkDir since Walk is now deprecated)
@@ -36,35 +31,9 @@ func GetFilePathRecursive(path string) ([]string, error) {
 		return nil
 	})
 
-	return paths, err
-}
-
-func ToCsv(excelPath string, csvPath string, sheetIndex int) (string, error) {
-	excel, err := excelize.OpenFile(excelPath)
 	if err != nil {
-		return csvPath, nil
-	}
-	defer func() {
-		if err := excel.Close(); err != nil {
-			log.Fatalln(err)
-		}
-	}()
-
-	rowsExcel, err := excel.GetRows(excel.GetSheetName(sheetIndex))
-	if err != nil {
-		log.Fatal("Failed to open Excel file : ", err)
+		log.Fatal("Failed to get the path to the Excel file : ", err)
 	}
 
-	if file.Exists(csvPath) {
-		rowsCsv := csv.LoadCsv(csvPath, false, false)
-		if reflect.DeepEqual(rowsExcel, rowsCsv) {
-			fmt.Println("[  SKIP  ] ", excelPath)
-			return "", nil
-		}
-	}
-
-	csv.NewFile(csvPath, rowsExcel)
-	fmt.Println("[COMPLETE] ", excelPath)
-
-	return csvPath, nil
+	return paths
 }
