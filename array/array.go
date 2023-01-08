7b0776744a050d7ab603e3ed4d93001ce6c0f3d3
stepupdream <stepupdream1002@gmail.com>
2023-01-08 13:57:28 +0900

    [add] Added method to determine uniqueness.

--- array/array.go
+++ array/array.go
@@ -38,7 +38,25 @@ func MergeMap(m1, m2 map[string]interface{}) map[string]interface{} {
 }
 
 func IsArrayUnique(args []int) bool {
+	return IsIntArrayUnique(args)
+}
+
+func IsIntArrayUnique(args []int) bool {
 	encountered := map[int]bool{}
+	count := len(args)
+	for i := 0; i < count; i++ {
+		if !encountered[args[i]] {
+			encountered[args[i]] = true
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
+
+func IsStringArrayUnique(args []string) bool {
+	encountered := map[string]bool{}
 	count := len(args)
 	for i := 0; i < count; i++ {
 		if !encountered[args[i]] {
